744ba4f208b0a198dcffd0af16bf921b76c3a1b1
Raj Sahani <124754600+raj15sahani@users.noreply.github.com>
2023-10-06 14:53:27 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+function calculate()
+{
+    let year;
+    let month;
+    let day;
+
+    let currentDay = parseInt(cd.value.slice(8,10),10);
+    let currentMonth = parseInt(cd.value.slice(5,7),10);
+    let currentYear = parseInt(cd.value.slice(0,4),10);
+    console.log("current date:-",currentYear,currentMonth,currentDay);
+
+    let birthDay = parseInt(dob.value.slice(8,10),10);
+    let birthMonth = parseInt(dob.value.slice(5,7),10);
+    let birthYear = parseInt(dob.value.slice(0,4),10);
+    console.log("dob:-",birthYear,birthMonth,birthDay);
+    
+    if(currentDay>=birthDay)
+    {
+        day=currentDay-birthDay;
+    }
+    else
+    {
+        day=currentDay + new Date(currentYear,currentMonth).getDate()-birthDay;
+        currentMonth--;
+    }
+
+    if(currentMonth>=birthMonth)
+    {
+        month=currentMonth-birthMonth;
+    }
+    else
+    {
+        month=currentDay+12-birthMonth;
+        currentYear--;
+    }
+
+   year=currentYear-birthYear;
+   if(year<0)
+   {
+        ageText.innerHTML = "ERROR";
+   }
+   else
+   {
+    ageText.innerHTML = year + "years," + month + "months," + day + "days";
+   }
+    
+
+}
